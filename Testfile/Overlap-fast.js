6d5555747d5c1bd799bb58d5a5ea6fa0b7ac9f3d
GuenoleLorho <1612807@rgu.ac.uk>
2020-04-21 22:14:38 +0100

    smooth with slider

--- Testfile/Overlap-fast.js
+++ Testfile/Overlap-fast.js
@@ -1,84 +1,53 @@
-var endVideo;
-var frameNo = 7;
-var frameNoString = ""+0+7;
+var cutId ="" + 0 + 8;
+
 
 window.addEventListener("load", setUp, false);
 
 function setUp(){
-	var video = document.getElementById('videotest')
-	video.src = 'videos/Group1_fast-video_short/07.mp4';
-	video.currentTime = 0.2;
+	var video = document.getElementById('playedVideo');
 	video.addEventListener("ended", replay);
-	checkFrame();
-
 	
+	
+	loadVid();
+	play();
+	slider();
+}
+function slider(){
+	var slider = document.getElementById("myRange");
+	slider.oninput = function() {
+		cutId = this.value;
+		if(cutId < 10){
+			cutId = ""+ 0 + cutId;
+		}
+	play();
+	}
 }
 
-function timeline(frame){
-	var selection = arguments[0];
-	frameNo = arguments[0];
-	if(selection < 10){
-		selection = ""+ 0 + selection;
-	}
+function play(){
 	document.getElementById("video-message").innerHTML = "";
-	var video = document.getElementById('videotest');
-	video.src = 'videos/Group1_fast-video_short/'+selection+'.mp4';
-	showHideBtn();
+	var video = document.getElementById("playedVideo");
+	video.src = 'videos/Group1_fast-video_short/'+cutId+'.mp4'
+	video.currentTime = 0;
 }
-
-function checkFrame(){
-	document.getElementById(frameNoString).checked = true;
-}
-
-function showHideBtn(){
-	if(frameNo == 00){
-		document.getElementById("button-left").style.display = "none";
-	}else{
-		document.getElementById("button-left").style.display = "inline";
-	}
-	if(frameNo == 15){
-		document.getElementById("button-right").style.display = "none";
-	}else{
-		document.getElementById("button-right").style.display = "inline";
-	}
-}
-
-
 
 function replay(){
-	console.log("replay!!! yay");
 	document.getElementById("video-message").innerHTML = "<img src='replay2.svg'>";
-	var video = document.getElementById('videotest');
-	var replayButton = document.getElementById('videotest');
-	replayButton.addEventListener("click", function(){playVid();});
-}
-function playVid(){
-	var video = document.getElementById('videotest');
-	video.currentTime = 0.2;
-	video.play();
-	document.getElementById("video-message").innerHTML = "";
-}
-
-function shift(frame){
-	console.log("ya shift " + arguments[0]);
-	frameNo += arguments[0];
-	console.log("frameNo = " + frameNo);
-	document.getElementById("video-message").innerHTML = "";
-	showHideBtn();
-	if(frameNo < 10){
-		frameNoString = ""+ 0 + frameNo;
-	}else{
-		frameNoString = frameNo;
-	}
-	console.log("frameNoString = "+ frameNoString);
-	var video = document.getElementById('videotest');
-	video.src = 'videos/Group1_fast-video_short/'+frameNoString+'.mp4';
-	video.currentTime = 0.2;
-	checkFrame();
 }
 
 function validate(){
-	console.log("saved frame = " + frameNoString);
-	sessionStorage.setItem("fast-cut", frameNoString);
+	console.log("saved frame = " + cutId);
+	sessionStorage.setItem("fast-cut", cutId);
 	window.location = "OverlapTest-slow.html";
+}
+
+function loadVid(){
+	for(i=0; i<16; i++){
+		var x = i;
+		if(x < 10){
+			x = ""+ 0 + x;
+		}
+		var video = document.getElementById("BC_load");
+		video.src = 'videos/Group1_fast-video_short/'+x+'.mp4'
+		video.load();
+	}
 }
--- Testfile/Overlap-slow.js
+++ Testfile/Overlap-slow.js
@@ -1,80 +1,53 @@
-var endVideo;
-var frameNo = 10;
-var frameNoString = 10;
+var cutId = 10;
+
 
 window.addEventListener("load", setUp, false);
 
 function setUp(){
-	var video = document.getElementById('videotest')
-	video.src = 'videos/Group2_Slow-video_short/12.mp4';
-	video.currentTime = 0.2;
+	var video = document.getElementById('playedVideo');
 	video.addEventListener("ended", replay);
-	checkFrame();
+	
+	
+	loadVid();
+	play();
+	slider();
+}
+function slider(){
+	var slider = document.getElementById("myRange");
+	slider.oninput = function() {
+		cutId = this.value;
+		if(cutId < 10){
+			cutId = ""+ 0 + cutId;
+		}
+	play();
+	}
 }
 
-function timeline(frame){
-	var selection = arguments[0];
-	frameNo = arguments[0];
-	if(selection < 10){
-		selection = ""+ 0 + selection;
-	}
+function play(){
 	document.getElementById("video-message").innerHTML = "";
-	var video = document.getElementById('videotest');
-	video.src = 'videos/Group2_Slow-video_short/'+selection+'.mp4';
-	showHideBtn();
+	var video = document.getElementById("playedVideo");
+	video.src = 'videos/Group2_Slow-video_short/'+cutId+'.mp4'
+	video.currentTime = 0;
 }
-
-function checkFrame(){
-	document.getElementById(frameNoString).checked = true;
-}
-
-function showHideBtn(){
-	if(frameNo == 00){
-		document.getElementById("button-left").style.display = "none";
-	}else{
-		document.getElementById("button-left").style.display = "inline";
-	}
-	if(frameNo == 18){
-		document.getElementById("button-right").style.display = "none";
-	}else{
-		document.getElementById("button-right").style.display = "inline";
-	}
-}
-
 
 function replay(){
 	document.getElementById("video-message").innerHTML = "<img src='replay2.svg'>";
-	var video = document.getElementById('videotest');
-	var replayButton = document.getElementById('videotest');
-	replayButton.addEventListener("click", function(){playVid();});
-}
-function playVid(){
-	var video = document.getElementById('videotest');
-	video.currentTime = 0.2;
-	video.play();
-	document.getElementById("video-message").innerHTML = "";
-}
-
-function shift(frame){
-	console.log("ya shift " + arguments[0]);
-	frameNo += arguments[0];
-	console.log("frameNo = " + frameNo);
-	document.getElementById("video-message").innerHTML = "";
-	showHideBtn();
-	if(frameNo < 10){
-		frameNoString = ""+ 0 + frameNo;
-	}else{
-		frameNoString = frameNo;
-	}
-	console.log("frameNoString = "+ frameNoString);
-	var video = document.getElementById('videotest')
-	video.src = 'videos/Group2_Slow-video_short/'+frameNoString+'.mp4';
-	video.currentTime = 0.2;
-	checkFrame();
 }
 
 function validate(){
-	console.log("saved frame = " + frameNoString);
-	sessionStorage.setItem("slow-cut", frameNoString);
+	console.log("saved frame = " + cutId);
+	sessionStorage.setItem("slow-cut", cutId);
 	window.location = "Questionnaire.html";
+}
+
+function loadVid(){
+	for(i=0; i<10; i++){
+		var x = i*2;
+		if(x < 10){
+			x = ""+ 0 + x;
+		}
+		var video = document.getElementById("BC_load");
+		video.src = 'videos/Group2_Slow-video_short/'+x+'.mp4'
+		video.load();
+	}
 }
